83e03c9bc19087530032c0f04fb1ef09a52ca1e7
felda <felda@felda.io>
2024-09-22 16:38:17 -0500

    lectures 01 to 13

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,35 @@
+//Assignment 01- Values and Variables
+const country = "USA";
+const continent = "North America";
+const population = 333;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Assignment 02- Data Types
+const isIsland = false;
+let language;
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//Assignment 03- let, const, and var
+language = "English";
+
+//Assignment 04- Basic Operators
+console.log(population / 2);
+console.log(population + 1);
+const finlandPop = 6;
+console.log(population == finlandPop);
+const averagePop = 33;
+console.log(population == averagePop);
+const description =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
